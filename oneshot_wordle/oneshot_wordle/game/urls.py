efaa5aa3c74f118d129181467765ec653732aa69
learnICT <kibrisli7@gmail.com>
2023-04-20 12:59:00 +0100

    error with statlloader fixed

--- oneshot_wordle/oneshot_wordle/game/urls.py
+++ oneshot_wordle/oneshot_wordle/game/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from oneshot_wordle.game.views import (
     wordle,
+    load_words,
 )
 
 app_name = "game"
 urlpatterns = [
     path("", wordle, name="home"),
+    path("load_words", load_words, name="load_words"),
 ]
