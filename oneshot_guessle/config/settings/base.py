867d34efedc66c096db5237fb0dfb091c1cb2af3
bitfez <kibrisli7@gmail.com>
2023-05-19 19:08:56 +0100

    2 staticfile dirs

--- oneshot_guessle/config/settings/base.py
+++ oneshot_guessle/config/settings/base.py
@@ -146,7 +146,7 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] #[str(APPS_DIR / "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),str(APPS_DIR / "static"),] #[str(APPS_DIR / "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
