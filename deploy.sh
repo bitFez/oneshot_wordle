44fea9136aa3f65a1754a19a9b3cfb0a0b4e2d5e
bitfez <kibrisli7@gmail.com>
2026-01-29 23:03:01 +0000

    added docker cleanup command to hetzner

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,7 @@ docker-compose -f production.yml up -d --build
 # run migrations and collectstatic
 docker-compose -f production.yml run --rm django python manage.py migrate --noinput
 docker-compose -f production.yml exec -T django python manage.py collectstatic --noinput
+
+# clean up unused docker resources to free up disk space
+# removes dangling images, stopped containers, unused networks, and build cache
+docker system prune -f --volumes
